d5faf517781978b996a8b4b9c176d73b0212f3ea
DenysenkoSerhii <107317701+DenysenkoSerhii@users.noreply.github.com>
2022-11-16 00:36:08 +0200

    3

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,13 @@
+import Vimeo from '@vimeo/player';
+import throttle from 'lodash.throttle';
 
+const startPlay = ({ seconds }) => {
+    localStorage.setItem('videoplayer-current-time', seconds);
+};
+
+const vimeoPlayerEl = document.querySelector('#vimeo-player');
+const player = new Vimeo(vimeoPlayerEl);
+
+ player.on('timeupdate', throttle(startPlay, 1000));
+
+player.setCurrentTime(localStorage.getItem('videoplayer-current-time'));
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,47 @@
+import throttle from 'lodash.throttle';
 
+const formEl = document.querySelector('.feedback-form');
+
+const checkLocalstorage = key => {
+  return !!localStorage.getItem(key);
+};
+
+const fillTheFieldsFromLocalStorage = (el, localStorageKey) => {
+  if (!checkLocalstorage(localStorageKey)) return;
+
+  const objFromStorage = JSON.parse(localStorage.getItem(localStorageKey));
+
+  for (const key in objFromStorage) {
+    if (Object.hasOwnProperty.call(objFromStorage, key)) {
+      el.elements[key].value = objFromStorage[key];
+    }
+  }
+};
+
+const onInput = (e, objFromStorage = {}) => {
+  const key = 'feedback-form-state';
+
+  if (checkLocalstorage(key))
+    objFromStorage = JSON.parse(localStorage.getItem(key));
+
+  objFromStorage[e.target.name] = e.target.value;
+  localStorage.setItem(key, JSON.stringify(objFromStorage));
+};
+
+const onSubmit = e => {
+  const key = 'feedback-form-state';
+
+  const resultToConsole = checkLocalstorage(key)
+    ? JSON.parse(localStorage.getItem(key))
+    : { email: '', message: '' }; 
+
+  console.log(resultToConsole);
+
+  e.preventDefault();
+  e.currentTarget.reset();
+  localStorage.removeItem(key);
+};
+
+fillTheFieldsFromLocalStorage(formEl, 'feedback-form-state');
+formEl.addEventListener('input', throttle(onInput, 500));
+formEl.addEventListener('submit', onSubmit);
